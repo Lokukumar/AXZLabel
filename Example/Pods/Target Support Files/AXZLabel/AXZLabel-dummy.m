ad492bc5c4e6b1fdd95e00af2a791e7497dd1d52
Lokesh Kumar <kumarlokesh097@gmail.com>
2020-08-09 19:07:01 +0530

    \Initial Commit

---
+++ Example/Pods/Target Support Files/AXZLabel/AXZLabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AXZLabel : NSObject
+@end
+@implementation PodsDummy_AXZLabel
+@end
---
+++ Example/Pods/Target Support Files/Pods-AXZLabel_Example/Pods-AXZLabel_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AXZLabel_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AXZLabel_Example
+@end
